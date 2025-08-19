1660c765c92efc777bf3634b11a6cabbb2f59a13
PK <pk@PKs-MacBook-Pro.local>
2025-08-19 15:20:33 +0100

    Added accesslist to s3bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
 }
+
+resource "aws_s3_bucket_ownership_controls" "mybucketownership" {
+  bucket = aws_s3_bucket.mybucket.id
+  
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mybucketpublicaccess" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "mybucketacl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.mybucketownership,
+    aws_s3_bucket_public_access_block.mybucketpublicaccess,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
